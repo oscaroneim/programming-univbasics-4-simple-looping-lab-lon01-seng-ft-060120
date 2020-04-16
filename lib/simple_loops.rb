25df954aa3ce245dbe7b1e8f0cb5f5670a97d535
Oscar Oneim <oscar4londres@gmail.com>
2020-04-16 16:09:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,5 +3,13 @@ counter = 1
 while counter <= 5 do
   puts string
   counter += 1 
+ end
 end
+
+def loop_message_n_times(string,n)
+  counter = 1
+  while counter <= n do
+    puts string(n)
+    counter += 1
+  end
 end
