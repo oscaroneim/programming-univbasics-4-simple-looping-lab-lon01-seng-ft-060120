a7e7e214229540f52cff6fada8802e879f36d5d7
Oscar Oneim <oscar4londres@gmail.com>
2020-04-16 16:36:40 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,36 @@
-# Write your methods here
+#require 'pry'
+
+def loop_message_five_times(string)
+counter = 1 
+while counter <= 5 do
+  puts string
+  counter += 1 
+ end
+end
+
+def loop_message_n_times(string,n)
+  counter = 1
+  while counter <= n do
+    puts string
+    counter += 1
+  end
+end
+
+def output_array(array)
+  counter = 1 
+  
+  while counter < array.length do
+    puts array 
+    counter += 1 
+  end
+end
+
+def return_string_array(array)
+  counter = 0 
+  new_array = []
+ while counter < array.length do 
+   new_array.push(array[counter].to_s)
+   counter += 1 
+  end
+  new_array
+ end
